314dd155e60d15a5af28644074fc02d4b24905d9
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-01-11 14:23:16 +0900

    従業員の投稿機能作成

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -3,15 +3,23 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.Employee;
+import com.example.exception.ResourceNotFoundException;
 import com.example.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/vi/")
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
 	@Autowired
@@ -21,5 +29,34 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployee(){
 		return employeeRepository.findAll();
+		
+	}
+	
+	//employee rest apiを作成
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	//idからemployeeを探す
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		//idからemployeeを検索　なければ例外がスローされる
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("存在しないId" + id));
+		return ResponseEntity.ok(employee);
+	}
+	
+	//update employee rest api
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		//idからemployeeを検索　なければ例外がスローされる
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("存在しないId" + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updateEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updateEmployee);
 	}
 }
+
